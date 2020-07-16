1063ddc3f783de8974feaacf42fbec125dd8a15c
XuBovey <xu.bovey@foxmail.com>
2020-07-16 15:07:42 +0800

    完成动态三角波

--- main.cpp
+++ main.cpp
@@ -2,11 +2,18 @@
 #include <QWidget>
 
 //#include "layoutDemo.h"
+#include "qwidgetdraw.h"
 
 int main(int argc, char * argv[])
 {
     QApplication a(argc, argv);
 
+    QWidgetDraw *drawWidget = new QWidgetDraw();
+
+    drawWidget->setWindowTitle("Name");
+    drawWidget->resize(600,150);
+
+    drawWidget->show();
 
     return a.exec();
 }
--- qwidgetdraw.h
+++ qwidgetdraw.h
@@ -7,7 +7,20 @@ class QWidgetDraw : public QWidget
 {
     Q_OBJECT
 public:
+    QTimer *timer;
+    QPixmap *map;
+    int x = 0;
+    int y = 0;
+    int dir = 0;
+    int cnt = 0;
+
     explicit QWidgetDraw(QWidget *parent = nullptr);
+
+    void draw(QPainter *painter);
+    void drawDemo(QPainter *painter);
+    void drawTriangle(QPainter *painter);
+    void drawTriangleNew(QPainter *painter);
+    void refresh();
 
 private:
     void paintEvent(QPaintEvent *event) override;
