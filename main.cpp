74507419925c9a016a7f96505d58cc3a6f987386
XuBovey <xu.bovey@foxmail.com>
2020-07-17 11:34:52 +0800

    完成波形动态绘制，界面拉伸问题解决

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <QApplication>
 #include <QWidget>
+#include <QVBoxLayout>
 
 //#include "layoutDemo.h"
 #include "qwidgetdraw.h"
@@ -8,12 +9,23 @@ int main(int argc, char * argv[])
 {
     QApplication a(argc, argv);
 
-    QWidgetDraw *drawWidget = new QWidgetDraw();
+    QWidgetDraw *drawWidgetEcgI = new QWidgetDraw();
+    QWidgetDraw *drawWidgetEcgII = new QWidgetDraw();
 
-    drawWidget->setWindowTitle("Name");
-    drawWidget->resize(600,150);
+    drawWidgetEcgI->setSizePolicy(QSizePolicy::Policy::Preferred, QSizePolicy::Policy::Preferred);
+    drawWidgetEcgII->setSizePolicy(QSizePolicy::Policy::Preferred, QSizePolicy::Policy::Preferred);
 
-    drawWidget->show();
+    QVBoxLayout *layoutMain = new QVBoxLayout();
+
+    layoutMain->addWidget(drawWidgetEcgI);
+    layoutMain->addWidget(drawWidgetEcgII);
+
+    QWidget *mainWin = new QWidget();
+    mainWin->setWindowTitle("TeamName 1600 x 600");
+    mainWin->setLayout(layoutMain);
+    mainWin->resize(1600, 600);
+
+    mainWin->show();
 
     return a.exec();
 }
--- qwidgetdraw.h
+++ qwidgetdraw.h
@@ -8,11 +8,13 @@ class QWidgetDraw : public QWidget
     Q_OBJECT
 public:
     QTimer *timer;
-    QPixmap *map;
+    QPixmap map;
     int x = 0;
     int y = 0;
     int dir = 0;
     int cnt = 0;
+    unsigned int index = 0;
+    int lastData = 0;
 
     explicit QWidgetDraw(QWidget *parent = nullptr);
 
@@ -20,13 +22,20 @@ public:
     void drawDemo(QPainter *painter);
     void drawTriangle(QPainter *painter);
     void drawTriangleNew(QPainter *painter);
+    void drawWaveFromArray(QPainter *painter);
+    void drawWave(QPainter *painter, int data);
+    void sendData();
     void refresh();
+    void refreshFromData(int data);
+    void testRx(int data);
 
 private:
     void paintEvent(QPaintEvent *event) override;
 
-signals:
+    void resizeEvent(QResizeEvent *event) override;
 
+signals:
+    void rxDataSignal(int data);
 };
 
 #endif // QWIDGETDRAW_H
